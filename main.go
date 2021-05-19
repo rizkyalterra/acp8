24a2703d25d83e2e47bf8fc73658f933bbbc56cf
Rizky <rizky.kurniawan@alterra.id>
2021-05-19 20:20:37 +0700

    change main.go

--- main.go
+++ main.go
@@ -4,26 +4,6 @@ import (
 	"fmt"
 )
 
-func FibTopDown(n int) int {
-	memo := map[int]int{
-		0: 0,
-		1: 1,
-	}
-	// 2 - 10
-	if val, exist := memo[n]; exist {
-		return val
-	} else {
-		return FibTopDown(n-1) + FibTopDown(n-2)
-	}
-	return memo[n]
-}
-
 func main() {
-	// var money int
-	// arrChange := []int{1, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000}
-	// sort.Sort(sort.Reverse(sort.IntSlice(arrChange)))
-	// fmt.Scanln(&money)
-
-	// fmt.Println("Kembalian: ")
-	fmt.Println(FibTopDown(7))
+	fmt.Println("Hello World")
 }
